0d2f326bbfaf8ca1d25b9a7d744de02b06b47b26
rizadwiandhika <rizadwiandhika@gmail.com>
2021-02-21 19:08:46 +0700

    Form validation finish

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import classes from './Input.module.css'
+
+/** Upload file ke firebase pake react
+ * youtube: react-firebase-file-uploader
+*/
+const input = ( props ) => {
+    let inputElement = null
+    let validationMessage = null
+    let inputElementClasses = [classes.InputElement]
+
+    if ( props.invalid && props.touched ) {
+        inputElementClasses.push( classes.Invalid )
+        validationMessage = <p className={ classes.ValidationError }>{props.invalidMessage}</p>
+    }
+    /** Attribute non-reserved pada ORIGINAL HTML TAG harus lowercase
+     * Pada versi React yang baru, penulisan HTML ditekankan lebih benar
+     * jadinya gak boleh ada attribute HTML yang camelcase, harus lowercase
+    */
+    switch( props.elementType ) {
+        case 'input' :
+            inputElement = (
+                <input {...props.elementConfig} 
+                        className={ inputElementClasses.join(' ') } 
+                        value={ props.value }
+                        onChange={ event => { 
+                            props.changed( props.inpKey, event ) 
+                        } }
+                        />
+            )
+            break
+
+        case 'select' :
+            const options = props.elementConfig.options.map( op => (
+                <option key={ op.value } value={ op.value }>{ op.displayValue }</option>
+            ) )
+            inputElement = (
+                <select 
+                    className={ inputElementClasses.join(' ') }
+                    value={ props.value }
+                    onChange={ event => { props.changed( props.inpKey, event ) } }>
+                    { options }
+                </select>
+            )
+            break
+
+        case 'textarea' :
+            inputElement = (
+                <textarea {...props.elementConfig} 
+                        className={ inputElementClasses.join(' ') } 
+                        value={ props.value }
+                        onChange={ () => {} }/>
+            )
+            break
+        
+        default :
+            inputElement = (
+                <input {...props.elementConfig} 
+                        className={ inputElementClasses.join(' ') } 
+                        value={ props.value }
+                        onChange={ () => {} }/>
+            )
+    }
+
+    return (
+        <div className={ classes.Input }>
+            <label className={ classes.Label }>{ props.label }</label>
+            { inputElement }
+            { validationMessage }
+        </div>
+    )
+}
+
+export default input
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,53 +1,190 @@
 import React, { Component } from 'react'
 import axiosOrder from '../../../axios-order'
 import Button from '../../../components/UI/Button/Button'
-import Spinner from '../../../components/UI/Spinner/Spinner';
+import Spinner from '../../../components/UI/Spinner/Spinner'
+import Input from '../../../components/UI/Input/Input'
 import classes from './ContactData.module.css'
 
 class ContactData extends Component {
 
     state = {
-        name : '',
-        email : '',
-        address : {
-            street : '',
-            postalCode : ''
+        orderForm : {
+            name : this.setInput('input', '', false,
+                        { type : 'text', placeholder : 'Enter your name' }, 
+                        { shouldBeChecked : true, required : true, alphabetOnly : true },
+                        'Please input correct name'),
+
+            street : this.setInput('input', '', false,
+                        { type : 'text', placeholder : 'Street' }, 
+                        { shouldBeChecked : true, required : true, }),
+
+            zipCode : this.setInput('input', '', false,
+                        { type : 'text', placeholder : 'Postal code' }, 
+                        { shouldBeChecked : true, required : true, minLength : 5, maxLength : 5 },
+                        'Postal code should be 5 characters'),
+
+            country : this.setInput('input', '', false,
+                        { type : 'text', placeholder : 'Country' }, 
+                        { shouldBeChecked : true, required : true, }),
+
+            email : this.setInput('input', '', false,
+                        { type : 'email', placeholder : 'email@example.com' },
+                        { shouldBeChecked : true, required : true, emailFormat : true },
+                        'Email is invalid'),
+
+            deliveryMethod : this.setInput('select', 'fastest', false,
+                                { options : [
+                                        {value : 'fastest', displayValue : 'Fastest' },
+                                        {value : 'cheapest', displayValue : 'Cheapest' }
+                                    ]
+                                },
+                                { shouldBeChecked : false })
         },
+        formIsValid : false,
         loading : false
+    }
+
+    /** Sebenernya buat check validasi form ada library-nya 
+     * salah satunya "formik".
+     * Di React gak ada built-in form vaildation seperti library Vue, Angular, dll.
+    */
+    setInput(elementType='input', value='', touched=false,
+                config={}, 
+                validation={shouldBeChecked : false},
+                invalidMessage='Input is not valid' ) {
+
+        let minReqValidation = {}
+
+        if ( validation.shouldBeChecked ) {
+            minReqValidation = {
+                ...validation,
+                status : false
+            }
+        } else {
+            touched = true
+            minReqValidation = {
+                shouldBeChecked : false,
+                status : true
+            }
+        }
+
+        return {
+            elementType : elementType,
+            value : value,
+            touched : touched,
+            elementConfig : config,
+            validation : minReqValidation,
+            invalidMessage : invalidMessage
+        }
+    }
+
+    /** Sebenernya buat check validasi form ada library-nya 
+    * salah satunya "formik".
+    * Di React gak ada built-in form vaildation seperti library Vue, Angular, dll.
+    */
+    checkValidity(value, rules) {
+        if ( ! rules.shouldBeChecked ) {
+            return true
+        }
+
+        let isValid = true
+
+        if ( rules.required ) {
+            isValid = value.trim() !== '' && isValid
+        }
+
+        if ( rules.alphabetOnly ) {
+            isValid = this.allLetter( value ) && isValid
+        }
+
+        if ( rules.emailFormat ) {
+            isValid = this.validateEmail( value ) && isValid
+        }
+
+        if ( rules.minLength ) {
+            isValid = value.length >= rules.minLength && isValid
+        }
+
+        if ( rules.maxLength ) {
+            isValid = value.length <= rules.maxLength && isValid
+        }
+
+        return isValid
+    }
+
+    allLetter( text ) { 
+        let letters = /^[a-zA-Z-,]+(\s{0,1}[a-zA-Z-, ])*$/ // Ini adalah regex
+
+        if ( text.match( letters ) ) { 
+            return true
+        }
+        return false
+    }
+
+    validateEmail( email )  {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+        if ( emailRegex.test( email ) ) {
+            return true
+        }
+        return false
+    }
+
+    formChangeHandler = (key, event) => {
+        this.setState( prevState => {
+            const newOrderForm = {...prevState.orderForm}
+            const newSelectedInput = {...newOrderForm[key]}
+            
+            newSelectedInput.touched = true
+            newSelectedInput.value = event.target.value
+            newSelectedInput.validation.status = this.checkValidity( 
+                                                    newSelectedInput.value, newSelectedInput.validation )
+            // 
+            newOrderForm[key] = newSelectedInput
+            
+            let formValid = true
+            for( const formField in newOrderForm ) {
+                formValid = newOrderForm[formField].validation.status && formValid
+            }
+
+            // newOrderForm[key] = this.setInput( elType, event.target.value, elConfig )
+
+            return { orderForm : newOrderForm, formIsValid : formValid }
+        } )
     }
 
     /** Kalo ada <button> dalam <form> saat kita klik
      * nanti jadinya bakal nge-reload app kita. jadinya perlu kita
      * preventDefault(). 
     */
-    orderHandler = () => {
+    orderHandler = (event) => {
+
+        event.preventDefault()
         this.setState({ loading : true })
 
-        const order = {
-            ingredients : this.props.ingredients,
-            price : this.props.price,
-            /** In production We should definitely calculate price on the server
-             * because we probably have our product stored in server 
-             * AND MAKE SURE USER ISN'T MANIPULATING THE CODE
-            */
-            customer : {
-                name : 'Riza Dwi Andhika',
-                address : {
-                    street : 'Jalan Ahmad Dahlan',
-                    zipCode : 50113,
-                    country : 'Indoneisa'
-                },
-                email : 'rizadwi@gmail.com'
-            },
-            deliveryMethod : 'fastest'
+        /** In production We should definitely calculate price on the server
+         * because we probably have our product stored in server 
+         * AND MAKE SURE USER ISN'T MANIPULATING THE CODE
+        */
+        const formData = {}
+
+        for ( const information in this.state.orderForm ) {
+            formData[information] = this.state.orderForm[information].value
         }
+
+        const order = {
+            orderDara : formData,
+            ingredients : this.props.ingredients,
+            price : this.props.price
+        }
+
         // Untuk firebase, nama node lalu .json
         axiosOrder
             .post( '/orders.json', order )
             .then( response => {
-                alert( 'Yeay!! Your order is being proccessed :)' )
+                // alert( 'Yeay!! Your order is being proccessed :)' )
                 this.setState({ loading : false })
-                this.props.history.push('/')
+                // this.props.history.push('/')
             })
             .catch( error => {
                 console.log( 'Dari catch: ' + error )
@@ -56,13 +193,39 @@ class ContactData extends Component {
             } )
     }
 
+    cancelHandler = (event) => {
+        event.preventDefault()
+    }
+
     render() {
+        let inputs = []
+
+        for ( const key in this.state.orderForm ) {
+
+            const input = this.state.orderForm[key]
+            const formated = <Input  
+                                key={ key }
+                                inpKey={ key }
+                                elementType={ input.elementType } 
+                                elementConfig={ input.elementConfig }
+                                value={ input.value }
+                                touched={ input.touched }
+                                invalid={ ! input.validation.status }
+                                invalidMessage={ input.invalidMessage }
+                                changed={ this.formChangeHandler } />
+
+            inputs.push( formated )
+        }
+
         let form = (
             <form>
-                <input type="text" name="name" placeholder="Your name" />
-                <input type="email" name="email" placeholder="Email" />
-                <input type="text" name="street" placeholder="Street" />
-                <input type="text" name="postal" placeholder="Postal code" />
+                { inputs }
+                <Button 
+                    disabled={ ! this.state.formIsValid } 
+                    btnType="Success" 
+                    clicked={ this.orderHandler.bind( this ) }>
+                    Order Now
+                </Button>
             </form>
         )
 
@@ -72,13 +235,11 @@ class ContactData extends Component {
             <div className={ classes.ContactData }>
                 <h4>Fill the data below for ordering: </h4>           
                 { form }
-                <div className={ classes.Confirmation }>
-                    <Button btnType="Danger" clicked={ () => {} }>Cancel</Button>
-                    <Button btnType="Success" clicked={ this.orderHandler }>Order Now</Button>
-                </div>
             </div>
         )
     }
+
+    
 }
 
 export default ContactData
